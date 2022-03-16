f1b3f4cfdd0d4ed935ca025cb12e03af3dcd869e
salwazpw <salwapw04@gmail.com>
2022-03-16 18:48:58 +0700

    Pembagian Section

---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,28 @@
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top ">
+    <div class="container d-flex align-items-center justify-content-lg-between">
+
+      <h1 class="logo me-auto me-lg-0"><a href="/home">Sanum<span>.</span></a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo me-auto me-lg-0"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <nav id="navbar" class="navbar order-last order-lg-0">
+        <ul>
+          <li><a class="nav-link scrollto active" href="/home">Home</a></li>
+          <li><a class="nav-link scrollto active" href="/about">About</a></li>
+          <li><a class="nav-link scrollto active" href="/services">Services</a></li>
+          <li class="dropdown "><a class="nav-link scrollto active" href="#"><span>List</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="/barang">Daftar Barang</a></li>
+              <li><a href="/pelanggan">Daftar Pelanggan</a></li>
+              <li><a href="/pegawai">Daftar Pegawai</a></li>
+              <li><a href="/suplier">Daftar Suplier</a></li>
+            </ul>
+          </li>
+          <li><a class="nav-link scrollto active" href="/contact">Contact</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
