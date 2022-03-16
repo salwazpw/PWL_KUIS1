0da2f6af574b2e4e2c02ad2748862960eea6d3f9
nazelahnm <hanumnazela21@gmail.com>
2022-03-16 19:39:59 +0700

    Pembenaran Model Pegawai.php

--- app/Models/Pegawai.php
+++ app/Models/Pegawai.php
@@ -1,34 +1,14 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+namespace App\Models;
 
-class CreatePegawaisTable extends Migration
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pegawai extends Model
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('Pegawais', function (Blueprint $table) {
-            $table->id();
-            $table->string('namaPegawai', 100)->index();
-            $table->string('noHp', 100)->index();
-            $table->string('posisi', 100)->index();
-            $table->timestamps();
-        });
-    }
+    use HasFactory;
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('Pegawais');
-    }
+    protected $table = 'Pegawais';
+    protected $primaryKey = 'idPegawai';
 }
