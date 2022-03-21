f2fab14c89391bc6c3ee56d4a9fc1dc10a4a05a1
salwazpw <salwapw04@gmail.com>
2022-03-21 08:39:39 +0700

    Penambahan Paginate

--- app/Http/Controllers/PelangganController.php
+++ app/Http/Controllers/PelangganController.php
@@ -8,7 +8,7 @@ use App\Models\Pelanggan;
 class PelangganController extends Controller
 {
     public function index(){
-        $data = Pelanggan::all();
+        $data = Pelanggan::paginate(5);
         return view('pelanggan', ['blog'=> $data], ['title'=>'Pelanggan']);
     }
 }
